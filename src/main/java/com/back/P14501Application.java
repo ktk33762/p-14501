b66fdf4375c8c834240027ba9b17bda195743b03
ktk3376 <ktk3376@gmail.com>
2026-05-04 14:14:01 +0900

    first commit

---
+++ src/main/java/com/back/P14501Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P14501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P14501Application.class, args);
+    }
+
+}
