4b08e273b89c9f766d4d6e8624bc861671710c17
ktk3376 <ktk3376@gmail.com>
2026-05-07 15:18:54 +0900

    26강, 수정, 샘플 데이터 5개 생성, @RequestParam 이용해서 디폴트 값 지정

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,6 +4,7 @@ import com.back.domain.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -14,11 +15,20 @@ import java.util.stream.Collectors;
 @Controller
 public class WiseSayingController {
     private int lastId = 0;
-    private final List<WiseSaying> wiseSayings = new ArrayList<>();
+    private final List<WiseSaying> wiseSayings = new ArrayList<>() {{
+        add(new WiseSaying(++lastId, "명언 1", "작가 1"));
+        add(new WiseSaying(++lastId, "명언 2", "작가 2"));
+        add(new WiseSaying(++lastId, "명언 3", "작가 3"));
+        add(new WiseSaying(++lastId, "명언 4", "작가 4"));
+        add(new WiseSaying(++lastId, "명언 5", "작가 5"));
+    }};
 
     @GetMapping("/wiseSayings/write")
     @ResponseBody
-    public String write(String content, String author) {
+    public String write(
+            @RequestParam(defaultValue = "내용") String content,
+            @RequestParam(defaultValue = "작가") String author
+    ) {
 
         if (content == null || content.isBlank()) {
             throw new IllegalArgumentException("content is empty");
@@ -67,5 +77,29 @@ public class WiseSayingController {
                 .stream()
                 .filter(wiseSaying -> wiseSaying.getId() == id)
                 .findFirst();
+    }
+
+    @GetMapping("/wiseSayings/modify/{id}")
+    @ResponseBody
+    public String modify(
+            @PathVariable int id,
+            @RequestParam(defaultValue = "") String content,
+            @RequestParam(defaultValue = "") String author
+    ) {
+        if (content.isBlank()) {
+            throw new IllegalArgumentException("content is empty");
+        }
+
+        if (author.isBlank()) {
+            throw  new IllegalArgumentException("author is empty");
+        }
+
+        WiseSaying wiseSaying = findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("%d번 명언은 존재하지 않습니다.".formatted(id))
+                );
+
+        wiseSaying.modify(content, author);
+
+        return "%d 번 명언이 수정되었습니다.".formatted(id);
     }
 }
