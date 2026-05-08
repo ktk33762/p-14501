46ab23773282d23c4e0f3dd44072323fcac7b8fb
ktk3376 <ktk3376@gmail.com>
2026-05-08 11:44:01 +0900

    37강, spring.datasource.hikari.autocommit 을 끈 상태로 @Transactional 여부에 따른 쿼리 비교

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,7 +4,6 @@ import com.back.domain.wiseSaying.entity.WiseSaying;
 import com.back.domain.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,7 +65,6 @@ public class WiseSayingController {
     }
 
 
-    @Transactional
     @GetMapping("/wiseSayings/{id}/delete")
     @ResponseBody
     public String delete(@PathVariable int id) {
