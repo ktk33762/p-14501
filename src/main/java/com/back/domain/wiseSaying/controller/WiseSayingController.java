c0568c2a0d1945f33be579f0984a3c4bb3b3ebbd
ktk3376 <ktk3376@gmail.com>
2026-05-08 11:17:08 +0900

    35강, 로그 출력 완료

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,6 +4,7 @@ import com.back.domain.wiseSaying.entity.WiseSaying;
 import com.back.domain.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,6 +66,7 @@ public class WiseSayingController {
     }
 
 
+    @Transactional
     @GetMapping("/wiseSayings/{id}/delete")
     @ResponseBody
     public String delete(@PathVariable int id) {
